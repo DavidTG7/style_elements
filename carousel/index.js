8bfffc95d7e5e73c4a851e50461bf98e4b984f8c
DavidTG7 <davidthekoa@gmail.com>
2022-05-30 23:56:49 -0500

    HTML and JS for Carousel project

---
+++ carousel/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+const main = document.querySelector('.main') 
+const dot = document.querySelectorAll('.dot')
+
+dot.forEach((oneDot, i) => {
+  dot[i].addEventListener('click', () => {
+
+    let position = i
+    let operation = position * -50
+
+    main.style.transform = `translateX(${ operation }%)`;
+
+  })
+})
